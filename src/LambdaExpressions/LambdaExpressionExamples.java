3851e379e84dfaa7667fc19cfc0c3d3e06d75702
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-08-21 19:57:26 -0700

    Lambda Expressions With Multiple Parameters

--- src/LambdaExpressions/LambdaExpressionExamples.java
+++ src/LambdaExpressions/LambdaExpressionExamples.java
@@ -4,11 +4,23 @@ interface Drawable0{
     public void draw();
 }
 
+interface Drawable1{
+    public void draw(int width);
+}
+
+interface Drawable2{
+    public void draw(int width, int height);
+}
+
 public class LambdaExpressionExamples {
     public static void main(String[] args){
         implementationWithoutLambdaExpression();
 
         implementationWithLambdaExpressionNoParameter();
+
+        implementationWithLambdaExpressionSingleParameter();
+
+        implementationWithLambdaExpressionMultipleParameters();
     }
 
     // Without Lambda Expression
@@ -34,4 +46,19 @@ public class LambdaExpressionExamples {
         drawable.draw();
     }
 
+    // With Lambda Expression And Single Parameter
+    public static void implementationWithLambdaExpressionSingleParameter(){
+        Drawable1 drawable = (width) -> {
+            System.out.println(String.format("implementationWithLambdaExpressionSingleParameter - Printing width %d in draw method!", width));
+        };
+        drawable.draw(25);
+    }
+
+    // With Lambda Expression And Multiple Parameters
+    public static void implementationWithLambdaExpressionMultipleParameters(){
+        Drawable2 drawable = (width, height) -> {
+            System.out.println(String.format("implementationWithLambdaExpressionMultipleParameters - Printing width %d and height %d in draw method!", width,height));
+        };
+        drawable.draw(30, 35);
+    }
 }
